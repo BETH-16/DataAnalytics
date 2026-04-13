54c20e6307028b0a8835dbd64a0344a310f93a1f
Betelehem teklemariaM <bteklemariam@my.yearupunited.org>
2026-04-13 14:47:19 -0400

    add EX1B AND EX1C fils

---
+++ week-02/Ex1c_sakile_exploration.sql
@@ -0,0 +1,11 @@
+/*
+a) it includes actorid first name lastname and last update
+b) film id title description release year languge id lengeth etc 
+c) film actor
+d) It contains rental id and date,inventory id,customerid and return date
+e) Inventory id,film id,store id and last update
+f) we need to use rental table gives the date and links to inventory and then ilm id all togetaher 
+*/
+
+SELECT ___________ FROM ____________;
+SELECT ___________ FROM ____________;
