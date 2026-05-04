4f429e4d623a5d2447f2a70c28e86f62d5ed177f
Betelehem teklemariaM <bteklemariam@my.yearupunited.org>
2026-05-04 14:44:49 -0400

    add the updated file

---
+++ week-05/EX1A_hello.py
@@ -0,0 +1,16 @@
+print('hello world!')
+
+message = ('hello world')
+print(message)
+
+# displaying dollars and cents
+dollars = 3
+cents = 0.50
+print (dollars + cents)
+# its gonna add the inte and float 
+cents = cents + 0.25
+print (dollars + cents)
+
+d_str = '3 dollars'
+c_str = '50 cents'
+print(d_str + " " + c_str)
