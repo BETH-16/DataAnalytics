76c458cb5f1cbf3118f56edecdfc4994279a4a07
Betelehem teklemariaM <bteklemariam@my.yearupunited.org>
2026-05-05 09:56:46 -0400

    add EX1B file

---
+++ week-05/EX1B_variable_examples.py
@@ -0,0 +1,28 @@
+customer_id = 611220
+customer_name = "katy brown"
+customer_gender = "female "
+customer_dob = "01-28-2010"
+driving_licensenumber ="MD6113345"
+auto_policynumber = 45676893
+
+print("Customer ID:", customer_id)
+print("Customer Name:", customer_name)
+print("Customer Gender:", customer_gender)
+print("Customer DOB:", customer_dob)
+print("Driving LicenseNumber:", driving_licensenumber)
+print("Auto Policy Number:", auto_policynumber)
+
+name = "Betelehem Teklemariam"
+city = "addis" 
+state = "addis"
+print(name)
+print(city)
+print(state)
+# a) False,	def	,if,raise,None,del,import,return
+# True,elif,in,try,and,else,is,while,as,except,lambda,with,assert,finally,nonlocal,yield,
+# break,for,not,class,form,or
+# b) 1.is This statement is used to check if the two variables are equal.
+#    2. while This statement is used to start a while ^^loop^^. It continues iteration until a condition is no longer True.
+#    3.as The as statement in Python re-assigns a returned object to a new identifier. Basically, it creates an ^^alias^^. 
+#    4.with The with statement is used to simplify the exception handling.
+#    5.assert The assert statement in Python is used for ^^debugging^^.
