d0126cab94f70756cfd2090dbb9af457fa573d62
Betelehem teklemariaM <bteklemariam@my.yearupunited.org>
2026-05-06 02:07:33 -0400

    add EX2A files

---
+++ week-05/EX2A_tips.py
@@ -0,0 +1,15 @@
+# Define values
+food_cost = 79.25
+tax = 6.54
+tip = 12.00
+
+# Calculste the unknown 
+total_due = food_cost + tax + tip
+
+# Display the results
+print("the total due is " + str(total_due))
+# the total due is 97.79
+print("food cost is" + str(food_cost) + "and tax is " + str(tax))
+#print("tip is " + str(tip))
+# food cost is79.25and tax is 6.54 tip is 97.79
+print("Tip is " + format(tip, ".2f"))
